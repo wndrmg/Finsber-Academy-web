d3030698fc0aa3685e9992ac66855533dfb3c17c
Wondermega <wondermega@finsber.com>
2023-01-11 02:50:28 +0300

    lendos mobilka gotova (bez desktopa)

--- src/App.tsx
+++ src/App.tsx
@@ -13,40 +13,46 @@ export const App = () => {
             </header>
             <main>
                 <div className='top-banner'>
-                    <h1>
-                        Курсы по финансам и&nbsp;инвестициям
-                    </h1>
-                    <ReactSVG className='top-banner-pic' src="covers/top-banner.svg" />
-                    <p>
-                        Научитесь управлять своими финансами. 
-                        Узнайте, как наращивать капитал и получать дополнительный доход за счет инвестиций
-                    </p>
-                    <button className='button button-blue'>Смотреть все курсы</button>
+                    <div className='top-banner-inner'>
+                        <h1>
+                            Курсы по финансам и&nbsp;инвестициям
+                        </h1>
+                        <ReactSVG className='top-banner-pic' src="covers/top-banner.svg" />
+                        <p>
+                            Научитесь управлять своими финансами. 
+                            Узнайте, как наращивать капитал и получать дополнительный доход за счет инвестиций
+                        </p>
+                        <button className='button button-blue'>
+                            Скачать приложение
+                        </button>
+                    </div>
                 </div>
                 <div className='main-content'>
                     <div className='cources-list'>
                         <h2>
                             Все курсы
                         </h2>
-                        <div className='cources-list-item'>
-                            <div className='item-cover' style={{  
-                                backgroundImage: 'url(' + 'covers/personal-finance.svg' + ')',
-                                backgroundPosition: 'center, bottom',
-                                backgroundRepeat: 'no-repeat'
-                            }}>
-                                <div className='item-badge'>
-                                    <span>Начинающим</span>
+                        <a href='#'>
+                            <div className='cources-list-item'>
+                                <div className='item-cover' style={{  
+                                    backgroundImage: 'url(' + 'covers/personal-finance.svg' + ')',
+                                    backgroundPosition: 'center, bottom',
+                                    backgroundRepeat: 'no-repeat'
+                                }}>
+                                    <div className='item-badge'>
+                                        <span>Начинающим</span>
+                                    </div>
+                                </div>
+                                <div className='item-description'>
+                                    <h3 className='item-title'>
+                                        Как привести личные финансы в порядок
+                                    </h3>
+                                    <div className='item-lessons-number'>
+                                        5 уроков
+                                    </div>
                                 </div>
                             </div>
-                            <div className='item-description'>
-                                <h3 className='item-title'>
-                                    Как привести личные финансы в порядок
-                                </h3>
-                                <div className='item-lessons-number'>
-                                    5 уроков
-                                </div>
-                            </div>
-                        </div>
+                        </a>
                         <div className='cources-list-item inactive'>
                             <div className='item-cover' style={{  
                                 backgroundImage: 'url(' + 'covers/fire-investments.svg' + ')',
