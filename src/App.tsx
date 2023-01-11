54673acf1c2d206d8ffab19f8e075256de7b3611
Wondermega <wondermega@finsber.com>
2023-01-11 23:54:53 +0300

    courses list adaptive ok

--- src/App.tsx
+++ src/App.tsx
@@ -29,31 +29,32 @@ export const App = () => {
                     </div>
                 </div>
                 <div className='main-content'>
+                    <h2>
+                        Все курсы
+                    </h2>
                     <div className='cources-list'>
-                        <h2>
-                            Все курсы
-                        </h2>
-                        <a href='#'>
-                            <div className='cources-list-item'>
-                                <div className='item-cover' style={{  
-                                    backgroundImage: 'url(' + 'covers/personal-finance.svg' + ')',
-                                    backgroundPosition: 'center, bottom',
-                                    backgroundRepeat: 'no-repeat'
-                                }}>
-                                    <div className='item-badge'>
-                                        <span>Начинающим</span>
-                                    </div>
-                                </div>
-                                <div className='item-description'>
-                                    <h3 className='item-title'>
-                                        Как привести личные финансы в порядок
-                                    </h3>
-                                    <div className='item-lessons-number'>
-                                        5 уроков
-                                    </div>
+                        
+                        {/* <a href='#'> */}
+                        <div className='cources-list-item'>
+                            <div className='item-cover' style={{  
+                                backgroundImage: 'url(' + 'covers/personal-finance.svg' + ')',
+                                backgroundPosition: 'center, bottom',
+                                backgroundRepeat: 'no-repeat'
+                            }}>
+                                <div className='item-badge'>
+                                    <span>Начинающим</span>
                                 </div>
                             </div>
-                        </a>
+                            <div className='item-description'>
+                                <h3 className='item-title'>
+                                    Как привести личные финансы в порядок
+                                </h3>
+                                <div className='item-lessons-number'>
+                                    5 уроков
+                                </div>
+                            </div>
+                        </div>
+                        {/* </a> */}
                         <div className='cources-list-item inactive'>
                             <div className='item-cover' style={{  
                                 backgroundImage: 'url(' + 'covers/fire-investments.svg' + ')',
