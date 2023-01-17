39e917cea7ebe9477af660d8980fafce47d834a9
Wondermega <wondermega@finsber.com>
2023-01-18 01:18:16 +0300

    hook - get data from api

---
+++ src/hooks/useCourses/useCourses.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react'
+import axios, {AxiosError} from 'axios'
+import {CourseData} from './model'
+
+export const useCourses = () => {
+    const [courses, setCourses] = useState<CourseData[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    async function fetchCourses() {
+        try {
+            setError('')
+            setLoading(true)
+            const response = await axios.get<CourseData[]>('/api/config')
+            setCourses(response.data)
+            setLoading(false)
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchCourses()
+    }, [])
+
+    return { courses, error, loading }
+}
