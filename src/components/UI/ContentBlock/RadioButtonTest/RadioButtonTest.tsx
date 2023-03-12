161a144b17ebc1d6368d94c6460c784c6b4d9144
Wondermega <wondermega@finsber.com>
2023-03-12 07:15:35 +0300

    radioButtonIsChecked bug fix

--- src/components/UI/ContentBlock/RadioButtonTest/RadioButtonTest.tsx
+++ src/components/UI/ContentBlock/RadioButtonTest/RadioButtonTest.tsx
@@ -3,7 +3,7 @@ import React, {useEffect, useRef, useState} from 'react';
 import {useTypedDispatch} from '@src/redux/store';
 import {useSelector} from 'react-redux';
 import {ContentBlockType} from '@src/redux/courses/courses.model';
-import {selectCoursesSelector, selectCurrentPage} from '@src/redux/courses/courses.selector';
+import {selectCoursesSelector, selectCurrentPage, selectRadioButtonIsCheckedSelector} from '@src/redux/courses/courses.selector';
 import {coursesActions} from '@src/redux/courses/courses.actions';
 
 import S from './RadioButtonTest.module.scss';
@@ -19,6 +19,8 @@ export const RadioButtonTest = ({content, blockIndex}: RadioButtonTestProps) => 
     const dispatch = useTypedDispatch();
     const courses = useSelector(selectCoursesSelector);
     const currentPage = useSelector(selectCurrentPage);
+    //без вызова селектора не работает вывод сообщения Верно/Неверно
+    const radioButtonIsChecked = useSelector(selectRadioButtonIsCheckedSelector);
     const currentCourse = courses.find((course) => course.id === urlParams.courseId);
     const currentLesson = currentCourse?.lessons.find((lesson) => lesson.id === urlParams.lessonId);
     const [isDispatched, setIsDispatched] = useState(false);
@@ -27,11 +29,11 @@ export const RadioButtonTest = ({content, blockIndex}: RadioButtonTestProps) => 
     const [isChecked, setIsChecked] = useState(false);
     useEffect(()=>{
         console.log('ref?.current?.checked', ref?.current?.checked);
-        setIsChecked(!!ref?.current?.checked)
+        console.log('radioButtonIsChecked', radioButtonIsChecked);
+        setIsChecked(!!ref?.current?.checked);
     }, [ref?.current?.checked])
     console.log('blockIndex', blockIndex);
     const onClickRadio = () => {
-        //console.log(content.text);
         dispatch(coursesActions.setRadioButtonIsCheckedCommit(content.text));
         if(isDispatched){
             return
@@ -63,7 +65,7 @@ export const RadioButtonTest = ({content, blockIndex}: RadioButtonTestProps) => 
                     name={blockIndex.toString()}
                     type='radio'
                     value={content.text}
-                    // checked={radioButtonIsChecked === content.text}
+                    //checked={radioButtonIsChecked === content.text}
                     onChange={onClickRadio}
                 />
                 {content?.text}
